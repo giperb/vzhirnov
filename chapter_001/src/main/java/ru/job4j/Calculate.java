1a4ee02890c48a5e073f2e526eac730e4f58ae2f
giperb <giperboloyd@gmail.com>
2017-03-28 12:01:37 +0300

    add JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+ * Программа Calculate.
+ *
+ * @author Vladimir Zhirnov (harabe@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
+	/**
+	* Метод main выводит приветствие и ничего не возвращает.
+	* @param args - массив строк.
+	**/
     public static void main(String[] args) {
     System.out.println("Hello world.");
     }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vladimir Zhirnov (mailto:harabe@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
