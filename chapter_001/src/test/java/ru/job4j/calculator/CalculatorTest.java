4999b7509e5118042a520751c3c783ee06aa2ede
giperb <giperboloyd@gmail.com>
2017-04-17 08:27:39 +0300

    uncomment CalculatorTest

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -27,34 +27,34 @@ public class CalculatorTest {
     /**
  	* Test subtract.
  	*/
- // 	@Test
-//     public void whenSubTwoMinOneThenOne() {
-//         Calculator calc = new Calculator();
-//         calc.add(2D, 1D);
-//         double result = calc.getResult();
-//         double expected = 1D;
-//         assertThat(result, is(expected));
-//     }
+	@Test
+    public void whenSubTwoMinOneThenOne() {
+        Calculator calc = new Calculator();
+        calc.subtract(2D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
     /**
  	* Test div.
  	*/
- // 	@Test
-//     public void whenDivFourToTwoThenTwo() {
-//         Calculator calc = new Calculator();
-//         calc.add(4D, 2D);
-//         double result = calc.getResult();
-//         double expected = 2D;
-//         assertThat(result, is(expected));
-//     }
+ 	@Test
+    public void whenDivFourToTwoThenTwo() {
+        Calculator calc = new Calculator();
+        calc.div(4D, 2D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
     /**
  	* Test multiple.
  	*/
-//  	@Test
-//     public void whenMultTwoToTwoThenFour() {
-//         Calculator calc = new Calculator();
-//         calc.add(2D, 2D);
-//         double result = calc.getResult();
-//         double expected = 4D;
-//         assertThat(result, is(expected));
-//     }
+ 	@Test
+    public void whenMultTwoToTwoThenFour() {
+        Calculator calc = new Calculator();
+        calc.multiple(2D, 2D);
+        double result = calc.getResult();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
 }
