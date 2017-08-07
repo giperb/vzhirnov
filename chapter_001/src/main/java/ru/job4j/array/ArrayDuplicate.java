2ef9ff1c7e69af0593987477357d5ae7ddb62144
giperb <giperboloyd@gmail.com>
2017-08-07 16:20:58 +0300

    autoformat ArrayDuplicate

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -8,24 +8,24 @@ import java.util.Arrays;
  * @since 26.07.2017
  * @version 0.1
  */
- public class ArrayDuplicate {
- 	/**
- 	 * The Method remove duplicate values.
- 	 * @param array - string array
- 	 * @return an array without duplicates
- 	 */
- 	public String[] remove(String[] array) {
-	int counter = 0;
+public class ArrayDuplicate {
+	/**
+	 * The Method remove duplicate values.
+	 * @param array - string array
+	 * @return an array without duplicates
+	 */
+	public String[] remove(String[] array) {
+		int counter = 0;
 		for (int i = 0; i < array.length - counter; i++) {
-	    	for (int j = i + 1; j < array.length - counter; j++) {
+			for (int j = i + 1; j < array.length - counter; j++) {
 				if (array[j].equals(array[i])) {
-		    	array[j] = array[array.length - 1 - counter];
-		    	array[array.length - 1 - counter] = array[j];
-		    	counter++;
-		    	j--;
+					array[j] = array[array.length - 1 - counter];
+					array[array.length - 1 - counter] = array[j];
+					counter++;
+					j--;
 				}
-	    	}
- 		}
- 	return Arrays.copyOf(array, array.length - counter);
- 	}
- }
+			}
+		}
+		return Arrays.copyOf(array, array.length - counter);
+	}
+}
